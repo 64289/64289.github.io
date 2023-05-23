d5bcc37c4b730278d2d73535b66132cc452aa4bd
Lokesh Kajala <80345471+64289@users.noreply.github.com>
2023-05-23 17:26:06 +0530

    Add files via upload

--- static/bootstrap/js/search.js
+++ static/bootstrap/js/search.js
@@ -1,1 +1,14 @@
-
+$(document).ready(function() {
+    $('#button-search').click(function() {
+      var searchQuery = $('input').val();
+      $.ajax({
+        url: '/search',
+        type: 'POST',
+        data: { 'search_query': searchQuery },
+        success: function(response) {
+          $('tbody').html(response);
+        }
+      });
+    });
+  });
+  
